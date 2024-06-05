9b17b98351e516532026198e2a7f6d02fa6ab166
harisankarsony <98963396+harisankarsony@users.noreply.github.com>
2024-06-05 22:20:47 +0530

    fixed checkbox bug.

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 let tasks = [];
+let activeButton = 'all';
 
 const input = document.querySelector('.input');
 const taskContainer = document.querySelector('.task-container');
 const taskList = document.querySelector('.task-list');
 const taskFooter = document.querySelector('.task-footer');
 
-const completedButton = document.querySelector('.completed');
-
 // render current tasks in the task-list.
 function showTasks(uncompletedAttr, activeButtonAttr = 'all') {
     let uncompleted = uncompletedAttr;
     const html = `${tasks.map(function (task, index) {
+        let id = task.id;
         let isDone = task.done;
         let checkedVal = 'unchecked';
         let textDecor = 'none';
@@ -18,7 +18,7 @@ function showTasks(uncompletedAttr, activeButtonAttr = 'all') {
             checkedVal = 'checked';
             textDecor = 'line-through';
         }
-        return `<li><input data='${index}' type="checkbox" class="checkbox" ${checkedVal} /><label style="text-decoration: ${textDecor};" class='label'>${task['text']}</label><button data='${index}' class="delete-button">x</button></li>`
+        return `<li><input data='${id}' type="checkbox" class="checkbox" ${checkedVal} /><label style="text-decoration: ${textDecor};" class='label'>${task['text']}</label><button data='${index}' class="delete-button">x</button></li>`
     }).join('')}`;
 
     taskContainer.style.display = 'block';
@@ -28,7 +28,6 @@ function showTasks(uncompletedAttr, activeButtonAttr = 'all') {
     let allOutlineStatus = 'none';
     let activeOutlineStatus = 'none';
     let completedOutlineStatus = 'none';
-    console.log(activeButtonAttr);
     switch (activeButtonAttr) {
         case 'active':
             activeOutlineStatus = '1px solid rgba(182, 0, 0, 0.747)';
@@ -50,15 +49,12 @@ function showTasks(uncompletedAttr, activeButtonAttr = 'all') {
 // add new task to task list
 input.addEventListener('keypress', function (event) {
     if (event.key === "Enter") {
-        if (input.value.length === 0) {
-            alert('Please add a non-empty task.');
-        }
-        else {
-            tasks.unshift({ text: input.value, done: 0 });
+        if (input.value.length !== 0) {
+            tasks.unshift({ text: input.value, done: 0, id: Date.now().toString() });
 
-            if(activeButton === 'completed')
+            if (activeButton === 'completed')
                 showCompletedTasks()
-            else if(activeButton === 'active')
+            else if (activeButton === 'active')
                 showActiveTasks();
             else
                 showAllTasks();
@@ -71,17 +67,26 @@ input.addEventListener('keypress', function (event) {
 // delete existing task from task list
 // mark completed task as done
 taskList.addEventListener('click', (e) => {
-    let index = e.target.getAttribute('data');
+
+    let currid = e.target.getAttribute('data');
+    let i;
+    tasks.forEach((task, index) => {
+        if (task.id === currid) {
+            i = index
+        }
+    });
+
     if (e.target.className === 'delete-button') {
-        tasks.splice(index, 1);
+        tasks.splice(i, 1);
     }
 
     else if (e.target.className === 'checkbox') {
-        tasks[index].done === 1 ? tasks[index].done = 0 : tasks[index].done = 1;
+        tasks[i].done === 1 ? tasks[i].done = 0 : tasks[i].done = 1;
     }
-    if(activeButton === 'completed')
-        showCompletedTasks()
-    else if(activeButton === 'active')
+
+    if (activeButton === 'completed')
+        showCompletedTasks();
+    else if (activeButton === 'active')
         showActiveTasks();
     else
         showAllTasks();
@@ -92,7 +97,6 @@ function getUnCompletedCount(tasks) {
     return tasks.filter(item => !item.done).length;
 }
 
-let activeButton = 'all';
 // show completed tasks
 function showCompletedTasks() {
     let uncompleted = getUnCompletedCount(tasks);
@@ -123,9 +127,9 @@ function showAllTasks() {
 // delete completed tasks
 function deleteCompletedTasks() {
     tasks = tasks.filter(task => task.done === 0);
-    if(activeButton === 'completed')
+    if (activeButton === 'completed')
         showCompletedTasks()
-    else if(activeButton === 'active')
+    else if (activeButton === 'active')
         showActiveTasks();
     else
         showAllTasks();
@@ -140,21 +144,21 @@ function deleteCompletedTasks() {
 
 let flag = 0;
 function markAllTasks() {
-    if(flag === 0) {
-        for(i of tasks) {
+    if (flag === 0) {
+        for (i of tasks) {
             i.done = 1;
         }
         flag = 1;
     }
     else {
-        for(i of tasks) {
+        for (i of tasks) {
             i.done = 0;
         }
         flag = 0;
     }
-    if(activeButton === 'completed')
+    if (activeButton === 'completed')
         showCompletedTasks()
-    else if(activeButton === 'active')
+    else if (activeButton === 'active')
         showActiveTasks();
     else
         showAllTasks();
